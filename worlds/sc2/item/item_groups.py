9340f6c7b09366641e6e47aa6b4d31f439151582
SirChuckOfTheChuckles <145928643+SirChuckOfTheChuckles@users.noreply.github.com>
2025-09-21 11:45:44 -0400

    typo

--- worlds/sc2/item/item_groups.py
+++ worlds/sc2/item/item_groups.py
@@ -754,7 +754,7 @@ item_name_groups[ItemGroupNames.STARGATE_UNITS] = stargate_units = [
     item_names.ARBITER, item_names.ORACLE, item_names.OPPRESSOR,
     item_names.CALADRIUS, item_names.MISTWING,
 ]
-item_name_groups[ItemGroupNames.STARGATE_UNITS] = nexus_units = [
+item_name_groups[ItemGroupNames.NEXUS_UNITS] = nexus_units = [
     item_names.MOTHERSHIP_AIUR, item_names.MOTHERSHIP_PURIFIER,
 ]
 item_name_groups[ItemGroupNames.PROTOSS_BUILDINGS] = protoss_buildings = [
