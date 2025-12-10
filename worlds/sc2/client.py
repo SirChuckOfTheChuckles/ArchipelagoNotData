13c64560a09b554a1d454b6faab63bebbaf710cf
SirChuckOfTheChuckles <cfnesi1023@gmail.com>
2025-12-10 09:40:35 -0500

    removed unnecessary lines

--- worlds/sc2/client.py
+++ worlds/sc2/client.py
@@ -1369,8 +1369,6 @@ API3_TO_API4_COMPAT_ITEMS: typing.Set[CompatItemHolder] = {
     CompatItemHolder(item_names.TEMPEST_DISINTEGRATION),
     CompatItemHolder(item_names.ARBITER_VESSEL_OF_THE_CONCLAVE),
     CompatItemHolder(item_names.MOTHERSHIP_TALDARIM_INTEGRATED_POWER),
-    CompatItemHolder(item_names.MOTHERSHIP_AIUR_VALOROUS_RESOLVE),
-    CompatItemHolder(item_names.MOTHERSHIP_PURIFIER_MOBILE_POWER_FIELD),
     # Other items
     CompatItemHolder(item_names.ASCENDANT_ARCHON_MERGE),
     CompatItemHolder(item_names.DARK_TEMPLAR_ARCHON_MERGE),
